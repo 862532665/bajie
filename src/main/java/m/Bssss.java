4c636f8c708659f89be3b4e2e1807a422166f6a3
862532665 <862532665@qq.com>
2019-03-11 18:05:05 +0800

    第一次提交

---
+++ src/main/java/m/Bssss.java
@@ -0,0 +1,12 @@
+package m;
+
+/**
+ * @author: 11827
+ * @date: 2019/3/11 18:03
+ * @description:
+ */
+public class Bssss {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
